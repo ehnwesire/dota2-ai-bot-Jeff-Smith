6696fcbcb3911bfffd01671ea11998b6353dbc29
EricLi <looking4eric@outlook.com>
2017-12-01 17:19:32 +0800

    updated hero selection script
    deleted the random selection part because we are starting the hero scripts with 5 comparatively easier ones. 
    will be added later.

--- hero_selection.lua
+++ hero_selection.lua
@@ -1,51 +1,27 @@
 function Think()
 
-team1 = {"npc_dota_hero_antimage", "npc_dota_hero_axe", "npc_dota_hero_pudge",
-"npc_dota_hero_crystal_maiden", "npc_dota_hero_puck"};
-team2 = {"npc_dota_hero_death_prophet", "npc_dota_hero_necrolyte", "npc_dota_hero_pugna",
-"npc_dota_hero_viper", "npc_dota_hero_skeleton_king"};
-team3 = {"npc_dota_hero_drow_ranger", "npc_dota_hero_earthshaker", "npc_dota_hero_legion_commander",
-"npc_dota_hero_silencer", "npc_dota_hero_obsidian_destroyer"};
-team4 = {"npc_dota_hero_pudge", "npc_dota_hero_viper", "npc_dota_hero_treant",
-"npc_dota_hero_venomancer", "npc_dota_hero_sven"};
-team5 = {"npc_dota_hero_sniper", "npc_dota_hero_drow_ranger", "npc_dota_hero_earthshaker",
+team1 = {"npc_dota_hero_skeleton_king", "npc_dota_hero_lina", "npc_dota_hero_axe",
+"npc_dota_hero_dazzle", "npc_dota_hero_crystal_maiden"};
+team2 = {"npc_dota_hero_sniper", "npc_dota_hero_drow_ranger", "npc_dota_hero_earthshaker",
 "npc_dota_hero_enigma", "npc_dota_hero_sandking"};
 
-teams = {team1, team2, team3, team4, team5};
 --5 different team setups, provided by jerry!
+--not anymore, it is eric's testing time :DDD
 --math.randomseed(tostring(os.time()):reverse():sub(1, 6))
-firstTeam = RandomInt(0,5);
-secondTeam = RandomInt(0,5);
-
-while (firstTeam==secondTeam ) 
-	do 
-		secondTeam=RandomInt(0,5); 
-	end
 	
 if ( GetTeam() == TEAM_RADIANT )
 	then
-		SelectHero(2,teams[firstTeam][1]);
-		SelectHero(3,teams[firstTeam][2]);
-		SelectHero(4,teams[firstTeam][3]);
-		SelectHero(5,teams[firstTeam][4]);
-		SelectHero(6,teams[firstTeam][5]);
+		SelectHero(2,team1[1]);
+		SelectHero(3,team1[2]);
+		SelectHero(4,team1[3]);
+		SelectHero(5,team1[4]);
+		SelectHero(6,team1[5]);
 	elseif ( GetTeam() == TEAM_DIRE )
 	then
-		SelectHero(7,teams[secondTeam][1]);
-		SelectHero(8,teams[secondTeam][2]);
-		SelectHero(9,teams[secondTeam][3]);
-		SelectHero(10,teams[secondTeam][4]);
-		SelectHero(11,teams[secondTeam][5]);
+		SelectHero(7,team2[1]);
+		SelectHero(8,team2[2]);
+		SelectHero(9,team2[3]);
+		SelectHero(10,team2[4]);
+		SelectHero(11,team2[5]);
 	end
 end
---[[
-LoadScript: X:\Steam\steamapps\common\dota 2 beta\game\dota\scripts\vscripts\bots\hero_selection.lua, scope name HeroSelectionScriptScope
-LoadScript: X:\Steam\steamapps\common\dota 2 beta\game\dota\scripts\vscripts\bots\team_desires.lua, scope name TeamDesiresScriptScope
-LoadScript: X:\Steam\steamapps\common\dota 2 beta\game\dota\scripts\vscripts\bots\team_desires.lua not found!
-LoadScript: X:\Steam\steamapps\common\dota 2 beta\game\dota\scripts\vscripts\bots\hero_selection.lua, scope name HeroSelectionScriptScope
-LoadScript: X:\Steam\steamapps\common\dota 2 beta\game\dota\scripts\vscripts\bots\team_desires.lua, scope name TeamDesiresScriptScope
-LoadScript: X:\Steam\steamapps\common\dota 2 beta\game\dota\scripts\vscripts\bots\team_desires.lua not found!
-Bot scripts reloaded.
---]]
-	
-	
